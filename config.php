ba190ce4dfba0afd85a672e35db734e90077111f
Сергей Ковтун <kovtunsergeyn@MacBook-Air-Sergej.local>
2016-06-25 19:08:43 +0300

    first

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'dbname' => 'erp_db',
+        'host' => 'localhost',
+        'user' => 'root',
+        'prefix' => 'cf_',
+        'password' => '1234',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'bWaQG8dfl8OzIi7HRsGkQKlJuafMCbvRFxtb.gFnzKLUIEIce7MoUHqzg5iJxxTC',
+  ),
+);
